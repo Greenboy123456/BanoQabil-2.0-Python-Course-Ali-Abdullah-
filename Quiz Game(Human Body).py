1503ddfbc260de686e4428c5bcecb7f70d0f3f4b
Greenboy123456 <156205679+Greenboy123456@users.noreply.github.com>
2024-02-09 22:41:45 +0500

    Add files via upload

---
+++ Quiz Game(Human Body).py
@@ -0,0 +1,295 @@
+#Welcome To Ali Abdullah Quiz Game
+print("Easy level")
+question1 = ("Q1. How many bones sre there in adult human body?")
+print(question1)
+your_for_question1 = input("Enter your awnser for question1:")
+awnser_of_question_1 = ("206")
+if your_for_question1 == awnser_of_question_1:
+    print("CORRECT!")
+    print("Score+1")
+else:
+    print("WRONG")
+    print("Score+0")
+question2 = ("Q2. What is the largest internal organ in a human body?")
+print(question2)
+your_for_question2 = input("Enter your awnser for question2:")
+
+awnser_of_question_2 = ("liver") 
+if your_for_question2 == awnser_of_question_2:
+        print("CORRECT!")
+        print("Score+1")
+else:
+     print("WRONG!")
+     print("Score+0")
+question3 = ("Q3. What is the smallest bone in a human body?")
+print(question3)
+your_for_question3 = input("Enter your awnser:")
+awnser_of_question_3 = ("stapes")
+if your_for_question3 == awnser_of_question_3:
+    print("CORRECT")
+    print("Score+1")
+else:
+    print("WRONG")
+    print("Score+0")
+question4 = ("Q4.How many chambers are there in the human heart?")
+print(question4)
+your_for_question4 = input("Enter your awnser:")
+awnser_of_question_4 = ("4") 
+if your_for_question4 == awnser_of_question_4:
+        print("CORRECT!")
+        print("Score+1")
+else:
+    print("WRONG!")
+    print("Score+0")
+question6 = ("Q6. How many lobes are there in the human brain?")
+print(question6)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("4")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question7 = ("Q7. How many pairs of ribs are there in human body?")
+print(question7)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("12")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question8 = ("Q8. What is the name of the process in which food converts into energy in the human body?")
+print(question8)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("metabolism")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question9 = ("Q9. What is the enzyme called that is found in saliva and helps in breaking down the food?")
+print(question9)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("amylase")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+print("Medium level")
+question10 = ("Q10. How many permanent teeth does a human usually have?")
+print(question10)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("32")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question11 = ("Q11. What is the largest organ in the human body?")
+print(question11)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("skin")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question12 = ("Q12. What is the name of the pigment that is responsible for human skin color?")
+print(question12)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("melanin")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question13 = ("Q13. What is the scientific name used for collar bone?")
+print(question13)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("clavicle")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question14 = ("Q14. What is the name of the sticky substance that helps in blood clotting?")
+print(question14)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("platelets")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question15 = ("Q15.  What is the name of the largest muscle in the human body?")
+print(question15)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("gluteus maximus")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question16 = ("Q16. What is the medical term used for thigh bone?")
+print(question16)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("femur")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question17 = ("Q17. What is the technical term used for kneecap?")
+print(question17)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("patella")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question18 = ("Q18. What is the name organ responsible for detoxifying harmful substances in the body?")
+print(question18)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("liver")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question19 = ("Q19. What is the medical term used for voice box?")
+print(question19)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("larynx")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+print("Hard level")
+question20 = ("Q20. What is the name of the pigment that is responsible for vision in low light conditions?")
+print(question20)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("rhodopsin")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question21 = ("Q21. What is the name of the hormone produced by the pancreas that regulates blood sugar levels?")
+print(question21)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("insulin")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question22 = ("Q22. What is the protective layer in brain called?")
+print(question22)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("meninges")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question23 = ("Q23. What is the medical term used for shin bone?")
+print(question23)
+your_awnser = input("Enter the answer for the given question:")
+awnser = ("tibia")
+if your_awnser == awnser:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question24 = ("Q24. What is the medical term used for windpipe?")
+print(question24)
+your_answer = input("Enter your answer for the given question:")
+answer = ("trachea")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question25 = ("Q25. Name the hormone that controls body metabolism")
+print(question25)
+your_answer = input("Enter your answer for the given question:")
+answer = ("thyroid")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question26 = ("Q26. Name the fluid the oxygen and nutrients to the body cells?")
+print(question26)
+your_answer = input("Enter your answer for the given question:")
+answer = ("blood")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question27 = ("Q27. What is the involuntary muscle that is responsible for breathing?")
+print(question27)
+your_answer = input("Enter your answer for the given question:")
+answer = ("diaphragm")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question28 = ("Q28. What is the smallest unit of fiber muscle?")
+print(question28)
+your_answer = input("Enter your answer for the given question:")
+answer = ("sarcomere")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question29 = ("Q29. What is the medical term used for chewing?")
+print(question29)
+your_answer = input("Enter your answer for the given question:")
+answer = ("mastication")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
+question30 = ("Q30. What is the main ingredient in the teeth?")
+print(question30)
+your_answer = input("Enter your answer for the given question:")
+answer = ("enamel")
+if your_answer == answer:
+     print("CORRECT")
+     print("Score+1")
+else:
+     print("WRONG")
+     print("Score+0")
